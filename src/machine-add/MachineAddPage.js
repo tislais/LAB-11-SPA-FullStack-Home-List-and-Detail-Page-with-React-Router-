dd632bc076f5a6348f8580b573bf536337d080c7
Tis Lais <tistististis@gmail.com>
2021-05-11 15:29:44 -0700

    add folders and basic components for new pages

---
+++ src/machine-add/MachineAddPage.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './MachineAddPage.css';
+
+export default class MachineAddPage extends Component {
+  
+  render() {
+    return (
+      <div className="MachineAddPage">
+        
+      </div>
+    );
+  }
+
+}
---
+++ src/machine-edit/MachineEditPage.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './MachineEditPage.css';
+
+export default class MachineEditPage extends Component {
+  
+  render() {
+    return (
+      <div className="MachineEditPage">
+        
+      </div>
+    );
+  }
+
+}
