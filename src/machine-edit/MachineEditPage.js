04500c0452ed86fe228f938df14080154b075f09
Tis Lais <tistististis@gmail.com>
2021-05-12 10:20:33 -0700

    fix redirect and unknown minor changes

--- src/machine-edit/MachineEditPage.js
+++ src/machine-edit/MachineEditPage.js
@@ -2,11 +2,14 @@ import { Component } from 'react';
 import './MachineEditPage.css';
 
 export default class MachineEditPage extends Component {
+  state = {
+    cat: null,
+    loading: true
+  }
   
   render() {
     return (
       <div className="MachineEditPage">
-        Hello
       </div>
     );
   }
